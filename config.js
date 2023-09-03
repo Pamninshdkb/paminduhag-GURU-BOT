525ae3bbafb77d0bf16adb40a957aacb44a41654
Pamninshdkb <143929831+Pamninshdkb@users.noreply.github.com>
2023-09-03 14:42:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94776224396', 'me', true],
+  ['+94776224396', 'pamindu', true], 
   [''] 
 ] //Number of owners
 
